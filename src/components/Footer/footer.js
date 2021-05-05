2d4c01e9a32a1a893fab4fc6cafe0d5682f9c9fd
subha-wp <subhapa98@gmail.com>
2021-05-05 16:02:00 +0530

    first commit

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Footer() {
+  return (
+    <section className="footer">
+      <div className="footer-container">
+        <p>
+          Developed by <a href="http://">Subha Patra </a>
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Footer
